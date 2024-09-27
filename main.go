e5e07237991f3ba9d3694db6939018a0bc59aa30
SaiRevanthSadhu <sairevanthsadhu@loyalistcollege.com>
2024-09-27 12:38:26 -0400

    Factorial

--- main.go
+++ main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Printf("The square root of given number is %f", sqrt_n(10))
+	var num int
+	fmt.Print("Enter a number: ")
+	fmt.Scan(&num)
+	fmt.Printf("Factorial of %d is %d\n", num, factorial(num))
 }
