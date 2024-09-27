2fe7c8885b045cca7134c7c69041383974aa1190
SachinTiwari986 <tiwari2sachin@gmail.com>
2024-09-27 00:51:01 -0400

    Square root function Done

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("The square root of given number is %f", sqrt_n(10))
+}
---
+++ sqrt_n.go
@@ -0,0 +1,9 @@
+package main
+
+import "math"
+
+func sqrt_n(a float64) float64 {
+
+	squareroot := math.Sqrt(a)
+	return squareroot
+}
